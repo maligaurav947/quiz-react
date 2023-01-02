b00b3fb7303539d4cb3c44edf1c4cf7fbf5fdd07
Mali Gaurav <maligaurav947@gmail.com>
2023-01-02 10:23:39 +0530

    V0.01

---
+++ src/components/Dashboard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <>
+      <section
+        className="bg-emerald-600 text-white"
+        style={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <main className="text-center grid gap-5">
+          <h1 className="text-5xl font-bold">LETS START QUIZE</h1>
+          <p className="text-3xl uppercase font-extralight">
+            HERE ARE category
+          </p>
+        </main>
+      </section>
+    </>
+  );
+}
+
+export default Dashboard;
